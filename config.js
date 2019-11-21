52da92d97c8c722db8d4d72facb94bbb261d07bc
Saad <m.saad.raza1999@gmail.com>
2019-11-20 21:14:35 -0500

    added a ttl for password reset links

--- config.js
+++ config.js
@@ -12,6 +12,7 @@ config.REDIS_HOST = 'localhost';
 config.SESSION_TTL = 1200;
 config.DIST_OVERLAP = 2;
 config.ADMIN_CREDS = { email: ['admin', 'brian.harrington@utsc.utoronto.ca'], password: ['UTSC_CS_admin321!', 'password'] };
+config.RESET_LINK_TTL = 30;
 // this variable is the path to the consent document that needs to be displayed when registering
 // NOTE: path is relative to being inside the static directory of the project
 config.CONSENT_TEXT = "/text/Consent_Form.pdf"
